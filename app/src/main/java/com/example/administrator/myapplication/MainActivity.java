c1ffacde26f9092d31774850da76bdd9a657ce90
guanyunxiu <374427398@qq.com>
2018-11-20 15:54:44 +0800

    关云秀测试2

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我是测试
+        //我是测试分支
     }
 }
