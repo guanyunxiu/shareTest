ac6037cfdbd42c25244f590ecac338555c34145b
guanyunxiu <374427398@qq.com>
2018-11-20 16:06:37 +0800

    测试

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //我是测试
-        //我是测试分支
+        //你好啊
+        //你知道吗
     }
 }
